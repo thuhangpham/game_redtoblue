8c06e73497b42be0cb523134d114bdaa16f7f5e4
Thu Hang <thuhangpt96@gmail.com>
2016-09-23 23:37:08 +0700

    Add project files.

---
+++ BlueToRed/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace BlueToRed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnOneMove = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblMoved = new System.Windows.Forms.Label();
+            this.lblCounted = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(531, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // btnOneMove
+            // 
+            this.btnOneMove.Enabled = false;
+            this.btnOneMove.Location = new System.Drawing.Point(85, 150);
+            this.btnOneMove.Name = "btnOneMove";
+            this.btnOneMove.Size = new System.Drawing.Size(75, 23);
+            this.btnOneMove.TabIndex = 3;
+            this.btnOneMove.Text = "One Move";
+            this.btnOneMove.UseVisualStyleBackColor = true;
+            this.btnOneMove.Click += new System.EventHandler(this.btnOneMove_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(198, 150);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblMoved
+            // 
+            this.lblMoved.AutoSize = true;
+            this.lblMoved.Enabled = false;
+            this.lblMoved.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblMoved.ForeColor = System.Drawing.Color.Red;
+            this.lblMoved.Location = new System.Drawing.Point(179, 64);
+            this.lblMoved.Name = "lblMoved";
+            this.lblMoved.Size = new System.Drawing.Size(24, 25);
+            this.lblMoved.TabIndex = 5;
+            this.lblMoved.Text = "0";
+            // 
+            // lblCounted
+            // 
+            this.lblCounted.AutoSize = true;
+            this.lblCounted.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblCounted.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblCounted.Location = new System.Drawing.Point(266, 64);
+            this.lblCounted.Name = "lblCounted";
+            this.lblCounted.Size = new System.Drawing.Size(24, 25);
+            this.lblCounted.TabIndex = 6;
+            this.lblCounted.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(531, 348);
+            this.Controls.Add(this.lblCounted);
+            this.Controls.Add(this.lblMoved);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnOneMove);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.Button btnOneMove;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblMoved;
+        private System.Windows.Forms.Label lblCounted;
+    }
+}
+
---
+++ BlueToRed/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlueToRed
+{
+    public partial class Form1 : Form
+    {
+        int cellSize = 100;
+        int countCell = 5, countMoved = 0;
+        int padingPanel = 12;
+        Panel panel1;
+        Label[] cellColor;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCell();
+        }
+        void InitializeCell()
+        {
+
+            if (countCell > 5)
+                cellSize = 70;
+            if (countCell > 8)
+                cellSize = 50;
+            cellColor = new Label[countCell * countCell];
+            panel1 = new Panel();
+            panel1.BackColor = Color.Lavender;
+            panel1.Location = new Point(padingPanel, padingPanel);
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.BackColor = Color.Peru;
+            panel1.Size = new Size(cellSize * countCell + countCell-1, cellSize * countCell + countCell-1);
+            int pos = 0;
+            for (int i = 0; i < countCell; i++)
+                for (int j = 0; j < countCell; j++)
+                {
+                    cellColor[pos] = new Label();
+                    cellColor[pos].Width = cellSize;
+                    cellColor[pos].Height = cellSize;
+                    cellColor[pos].BackColor = Color.Blue;
+                    cellColor[pos].Location = new Point(j * (cellSize + 1), i * (cellSize + 1));
+                    panel1.Controls.Add(cellColor[pos]);
+                    pos++;
+                }
+            this.Controls.Add(panel1);
+            btnPlay.Location = new Point(padingPanel, panel1.Height + 20);
+            btnOneMove.Location = new Point(btnPlay.Location.X + 90, panel1.Height + 20);
+            this.Size = new Size(padingPanel * 2 + panel1.Width + 10+20, panel1.Height + padingPanel + 50 + 30 + 20);
+            btnPlay.Enabled = true;
+            lblMoved.Location = new Point(countCell * cellSize / 2, btnPlay.Location.Y);
+            lblCounted.Location = new Point((countCell - 1) * cellSize, btnPlay.Location.Y);
+            if (int.Parse(lblMoved.Text) != 1 && int.Parse(lblCounted.Text) > int.Parse(lblMoved.Text) || lblCounted.Text == "0")
+                lblCounted.Text = lblMoved.Text;
+            btnOneMove.Enabled = false;
+        }
+        private void picColorClick(object sender, EventArgs e)
+        {
+            int sumCell = countCell * cellSize + countCell;
+            Label picColor = (Label)sender;
+            int k = (picColor.Location.Y % cellSize) * countCell +
+                 (picColor.Location.X % cellSize);
+            ChangeColor(picColor);
+            if ((k % countCell) != 0)
+                ChangeColor(cellColor[k - 1]);
+            if (k < cellColor.Length - 1 && ((k + 1) % countCell) != 0)
+                ChangeColor(cellColor[k + 1]);
+            if (k < cellColor.Length - countCell)
+                ChangeColor(cellColor[k + countCell]);
+            if (k >= countCell)
+                ChangeColor(cellColor[k - countCell]);
+            lblMoved.Text = (++countMoved).ToString();
+            Check();
+        }
+        void ChangeColor(Label a)
+        {
+            if (a.BackColor == Color.Red)
+                a.BackColor = Color.Blue;
+            else
+                a.BackColor = Color.Red;
+        }
+
+        private void btnOneMove_Click(object sender, EventArgs e)
+        {
+            int max = cellColor.Length;
+            for (int i = 0; i < max; i++)
+                cellColor[i].BackColor = Color.Blue;
+            Random rnd = new Random();
+            int k = rnd.Next(max - 1);
+            cellColor[k].BackColor = Color.Red;
+            if ((k % countCell) != 0)
+                cellColor[k - 1].BackColor = Color.Red;
+            // o cuoi cung va o bien phai
+            // random chay tu 0 -> n-1
+            // k+1 -> so o dc chia het.
+            if (k < cellColor.Length - 1 && ((k + 1) % countCell) != 0)
+                cellColor[k + 1].BackColor = Color.Red;
+            // o phia duoi
+            if (k < cellColor.Length - countCell)
+                cellColor[k + countCell].BackColor = Color.Red;
+            // o phia tren
+            if (k >= countCell)
+                cellColor[k - countCell].BackColor = Color.Red;
+        }
+        void Check()
+        {
+            int d = 0;
+            for (int i = 0; i < cellColor.Length; i++)
+                if (cellColor[i].BackColor == Color.Blue)
+                    d++;
+            if (d == cellColor.Length)
+            {
+                btnPlay.Enabled = true;
+                btnOneMove.Enabled = false;
+                MessageBox.Show("Chúc mừng bạn đã chiền thắng.\n Click Play để chơi nữa nhé!^_^ ", "Cung sỉ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Controls.Remove(panel1);
+                countMoved = 0;
+                InitializeCell();
+            }
+        }
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            lblMoved.Enabled = true;
+            btnPlay.Enabled = false;
+            lblMoved.Text = "0";
+            int t = cellColor.Length - 1;
+            while (t >= 0)
+            {
+                Application.DoEvents();
+                cellColor[t].BackColor = Color.Red;
+                t--;
+                Thread.Sleep(5);
+            }
+            btnOneMove.Enabled = true;
+            for (int i = 0; i < countCell * countCell; i++)
+            {
+                cellColor[i].Click += picColorClick;
+            }
+        }
+      
+    }
+}
